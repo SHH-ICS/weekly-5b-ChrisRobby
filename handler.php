d356e4830fa35333a30cfc9ec47f170552ef4d75
ChrisRobby <144351829+ChrisRobby@users.noreply.github.com>
2024-01-03 22:00:53 -0500

    Modify handler.php

--- handler.php
+++ handler.php
@@ -4,18 +4,26 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
+    <title>Results Page</title>
   </head>
 
   <body>
     
     <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+    function calcPie(int $n):float {
+      $pie = 3.14159;
+      return $pie;
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
+
+    $pi = 3.14159;
+    $number = "";
+    if ( isset( $_POST['number'] ) ){
+      $number = $_POST['number'];
+    }
+    echo "<h1>My Calculation of PI</h1>\n";
+    echo "<p>The number of iterations is ".$number."</p>\n";
+    echo "<p>The calculated value of PI is ".calcPie($number)."</p>\n";
+    echo "<p>The actual value of PI is ".$pi."</p>\n";
     ?>
     
   </body>
